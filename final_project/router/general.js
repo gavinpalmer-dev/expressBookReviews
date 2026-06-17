2c9fee89b3819d442b280e0277d77ea0f711dba7
gavinchigallis <gavin_palmer_gp@yahoo.com>
2026-06-17 00:07:22 -0500

    Task 06

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,20 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password are required"});
+  }
+
+  const existingUser = users.find(user => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({message: "Username already exists"});
+  }
+
+  users.push({ username, password });
+  return res.status(200).json({message: "User successfully registered"});
 });
 
 // Get the book list available in the shop
