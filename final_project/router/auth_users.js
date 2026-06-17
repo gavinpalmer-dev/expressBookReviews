ecf4c2bfa762bba14914362d332ed0ffcd4d03dd
gavinchigallis <gavin_palmer_gp@yahoo.com>
2026-06-17 00:46:28 -0500

    Task 09

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -52,6 +52,24 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({message: "Review added/modified successfully"});
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.user.data;
+
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({message: "Book not found"});
+  }
+
+  if (!book.reviews[username]) {
+    return res.status(404).json({message: "Review not found"});
+  }
+
+  delete book.reviews[username];
+  return res.status(200).json({message: "Review deleted successfully"});
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
