5d110e586668f66894912d625a7f7b9d136a304e
gavinchigallis <gavin_palmer_gp@yahoo.com>
2026-06-16 23:53:27 -0500

    Task 03

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const matchingBooks = {};
+  const bookKeys = Object.keys(books);
+  for (let key of bookKeys) {
+    if (books[key].author === author) {
+      matchingBooks[key] = books[key];
+    }
+  }
+  if (Object.keys(matchingBooks).length > 0) {
+    return res.status(200).json(matchingBooks);
+  }
+  return res.status(404).json({message: "No books found for the given author"});
 });
 
 // Get all books based on title
